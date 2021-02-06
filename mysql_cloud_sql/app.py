b2c8becfc322d6d15e1a4a88ebbf56ca8a27e7d9
Suyash <suyashdayal@gmail.com>
2021-02-07 02:35:36 +0530

    Configure flask_restful to enable development of REST APIs in app.py

--- mysql_cloud_sql/app.py
+++ mysql_cloud_sql/app.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 
@@ -10,6 +11,7 @@ load_dotenv(verbose=True)
 
 # Initializing Flask app
 app = Flask(__name__)
+api = Api(app)
 
 # Google Cloud SQL
 DB_USER = os.getenv('DB_USER')
@@ -20,19 +22,22 @@ DATABASE_NAME = os.getenv('DATABASE_NAME')
 
 # SQLAlchemy Configuration
 
-# To use mysqlconnector as the MySQL Driver, we need to install it using pip
-# and executing the following command in the terminal:
-# pip install mysql-connector-python
-
+# Using the official MySQL Driver for Python (developed by Oracle), 
+# mysqlconnector, to connect to the remote MySQL database 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DB_USER}:{DB_USER_PWD}@{DB_PUBLIC_IP_ADDRESS}/{DATABASE_NAME}?unix_socket=/cloudsql/{CONNECTION_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Using AutoMap to utilize tables already existing in the database
+# without the need to create our own Model classes.
 Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 Member = Base.classes.members
 
+# NOTE: We can't use query direcly on the classes mapped to tables in
+#       an existing database. To run queries, we need to use the
+#       query(mapped_table) on the db.session object.
 # Add a new member
 # new_member = Member(name="Vishy Anand", email="thevish@chess.com")
 # db.session.add(new_member)

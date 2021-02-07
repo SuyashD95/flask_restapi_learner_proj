ab25b63241cd93ecefa3a746725a7a94b602dbda
Suyash <suyashdayal@gmail.com>
2021-02-07 16:26:05 +0530

    Add unit tests to check PATCH request handler is working as per specs

--- mysql_cloud_sql/test.py
+++ mysql_cloud_sql/test.py
@@ -55,8 +55,27 @@ new_data = {'name': 'Mouse Mini', 'email': 'minimouse@email.com'}
 response = requests.put(f'{BASE_URL}{endpoint}', data=new_data)
 print(pprint_json(response))
 input()
-# OK (200) Success 
+# Status OK (200) Success 
 updated_data = {'name': 'Mini', 'email': 'minimouse@disney.com'}
 response = requests.put(f'{BASE_URL}{endpoint}', data=updated_data)
+print(pprint_json(response))
+input()
+
+# Testing PUT request of MemberEntity
+endpoint = 'members/20/update'
+# Bad Request (400) Error
+illegal_data = {'name': 'A valid name', 'email': 'A valid email', 'extra_arg': 'This is invalid'}
+response = requests.patch(f'{BASE_URL}{endpoint}', data=illegal_data)
+print(pprint_json(response))
+input()
+# Status OK (200) Success
+unreachable_endpoint = '/members/100/update'
+correct_data = {'name': 'Mouse'}
+response = requests.patch(f'{BASE_URL}{unreachable_endpoint}', data=correct_data)
+print(pprint_json(response))
+input()
+# Status OK (200) Success
+patch_data = {'name': 'Mini Mouse'}
+response = requests.patch(f'{BASE_URL}{endpoint}', data=patch_data)
 print(pprint_json(response))
 input()
